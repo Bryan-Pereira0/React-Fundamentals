e2c720550aa464d71e7a726ebb159fc54d54f1b8
bryanpereira <gfxmagicz@gmail.com>
2025-02-08 18:59:11 -0500

    React Fundamentals complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AboutSection from './assets/AboutSection.jsx';
+import ContactSection from './assets/ContactSection.jsx';
+import Header from './assets/Header.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <AboutSection/>
+      <ContactSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/ContactSection.jsx
@@ -0,0 +1,10 @@
+function ContactSection() {
+  return (
+    <footer>
+      <h1>Contact Us</h1>
+      <p>Phone: 123-456-7890</p>
+      <p>Email: TotallyReal@Email.com</p>
+    </footer>
+    );}
+
+export default ContactSection;
